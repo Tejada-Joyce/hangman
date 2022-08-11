6f09a766fe4a46ea5eb111b9c5df2d016a699148
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-08-10 23:45:25 -0600

    adjusted movie name split to wrap words

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,22 @@
 import { Fragment, useEffect, useState } from 'react';
 import styled from 'styled-components';
 
-const Letter = styled.div`
-  display: inline-block;
+const Letter = styled.span`
   border-bottom: 2px solid black;
   text-align: center;
   margin: 10px;
   width: 10px;
 `;
 
+const WordsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 95%;
+  column-gap: 20px;
+  justify-content: center;
+  align-items: center;
+  line-height: 40px;
+`;
 const Movie = ({ gaveUp, guessedLetter, movieName, setStrikes, setWon }) => {
   const [correctGuesses, setCorrectGuesses] = useState([]);
   useEffect(() => {
@@ -34,31 +42,36 @@ const Movie = ({ gaveUp, guessedLetter, movieName, setStrikes, setWon }) => {
     }
   }, [correctGuesses, movieName, setWon]);
 
+  const words = movieName.split(' ');
+
   return (
-    <div>
-      {[...movieName].map((letter, idx) => {
-        if (/[a-zA-Z]/.test(letter)) {
-          return (
-            <Fragment key={`letter_${idx}`}>
-              {gaveUp || correctGuesses.includes(letter.toLowerCase()) ? (
-                <Letter>{letter}</Letter>
-              ) : (
-                <Letter>-</Letter>
-              )}
-            </Fragment>
-          );
-        } else {
-          return (
-            <span
-              key={`_${idx}`}
-              style={{ marginLeft: letter === ' ' ? 25 : 0 }}
-            >
-              {letter}
-            </span>
-          );
-        }
+    <WordsContainer>
+      {words.map((word, idx) => {
+        return (
+          <div key={`${word}_${idx}`} style={{ display: 'inline-block' }}>
+            {[...word].map((letter, i) => {
+              if (/[a-zA-Z]/.test(letter)) {
+                return (
+                  <Fragment key={`letter_${idx}_${i}`}>
+                    {gaveUp || correctGuesses.includes(letter.toLowerCase()) ? (
+                      <Letter>{letter}</Letter>
+                    ) : (
+                      <Letter>-</Letter>
+                    )}
+                  </Fragment>
+                );
+              } else {
+                return (
+                  <span key={`_${idx}_${i}`} style={{ marginLeft: 0 }}>
+                    {letter}
+                  </span>
+                );
+              }
+            })}
+          </div>
+        );
       })}
-    </div>
+    </WordsContainer>
   );
 };
 
